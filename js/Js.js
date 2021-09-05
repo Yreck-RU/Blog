91c1514c2acd46be250667053fe6afff0894fc20
Yreck-RU <74424990+Yreck-RU@users.noreply.github.com>
2021-09-05 22:52:59 +0300

    Add files via upload
    gxfgnxgn

---
+++ js/Js.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	$('.heder__burger').click(function(event) {
+		$('.heder__burger,.heder_menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+function ibg(){
+		let ibg=document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if(ibg[i].querySelector('img')){
+			ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+		}
+	}
+}
+ibg();
